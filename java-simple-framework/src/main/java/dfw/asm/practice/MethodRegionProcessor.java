fa50e6890ad86a389d28fc7ecb8adb6a4f20f928
katlog <1375049265@qq.com>
2018-12-24 02:15:09 +0800

    test reflect

--- java-simple-framework/src/main/java/dfw/asm/practice/MethodRegionProcessor.java
+++ java-simple-framework/src/main/java/dfw/asm/practice/MethodRegionProcessor.java
@@ -1,12 +1,18 @@
 package dfw.asm.practice;
 
+import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
+
+import java.io.IOException;
 
 import static org.objectweb.asm.Opcodes.ASM4;
 
 public class MethodRegionProcessor {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        ClassReader cr = new ClassReader("java.lang.String");
+
 
     }
 
--- java-simple-framework/src/main/java/dfw/quartz/simple/HelloQuartzScheduling.java
+++ java-simple-framework/src/main/java/dfw/quartz/simple/HelloQuartzScheduling.java
@@ -1,5 +1,4 @@
-package dfw.quartz.simple;/*package org.person.dfw.quartz.simple;
-
+package dfw.quartz.simple;
 import org.quartz.CronScheduleBuilder;
 import org.quartz.Job;
 import org.quartz.JobBuilder;
@@ -35,9 +34,6 @@ public class HelloQuartzScheduling {
             .withSchedule(CronScheduleBuilder.cronSchedule("0/2 * * * * ?") //每2秒执行一次
                 )
         .build();
-                //每2秒执行一次
-            .withSchedule(CronScheduleBuilder.cronSchedule("0/2 1/3 * * * ?"))
-        .build();;
 
         //注册并进行调度
         scheduler.scheduleJob(jobDetail,trigger);
