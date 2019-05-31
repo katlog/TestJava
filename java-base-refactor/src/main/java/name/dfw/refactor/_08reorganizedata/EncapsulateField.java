ffafc0259714d4a94719d9d50fe11ecdeae8ebc3
katlog <1375049265@qq.com>
2019-06-01 04:08:29 +0800

    refactor

--- java-base-refactor/src/main/java/name/dfw/refactor/_08reorganizedata/EncapsulateField.java
+++ java-base-refactor/src/main/java/name/dfw/refactor/_08reorganizedata/EncapsulateField.java
@@ -3,6 +3,20 @@ package name.dfw.refactor._08reorganizedata;
 /**
  * Created by fw on 2018/4/24
  * 封装值域
+ *  class中存在一个public值域。将它声明为private，并提供相应的访问函数（accessors）
  */
 public class EncapsulateField {
+
+    class Demo{
+        class Before{
+            public String _name;
+        }
+        class Refactor{
+            private String _name;
+            public String getName() {return _name;}
+            public void setName(String arg) {_name = arg;}
+        }
+    }
+
+
 }
