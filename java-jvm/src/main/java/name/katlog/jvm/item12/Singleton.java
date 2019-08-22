54b819efd06832e3179ed5e26880ea2454819e32
difengwei <difengwei@58ganji.com>
2019-08-22 19:35:59 +0800

    jvm:concurrence chapter

---
+++ java-jvm/src/main/java/name/katlog/jvm/item12/Singleton.java
@@ -0,0 +1,19 @@
+package name.katlog.jvm.item12;
+
+public class Singleton {
+    private volatile static Singleton instance;
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+    public static void main(String[] args) {
+        Singleton.getInstance();
+    }
+}
