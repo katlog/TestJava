ea5461f77fc34b5d88d70d8ca4dbb294584d3cd1
difengwei <difengwei@58ganji.com>
2019-09-18 10:49:03 +0800

    base:jdbc

--- java-base-study/src/main/java/org/person/dfw/jdbc/TestSQL.java
+++ java-base-study/src/main/java/org/person/dfw/jdbc/TestSQL.java
@@ -29,8 +29,7 @@ import java.util.Properties;
 public class TestSQL {
 	
 	private static Properties pro;
-	
-	
+
 	private static String url;
 	private static String username;
 	private static String password;
@@ -42,15 +41,28 @@ public class TestSQL {
 	static{
 		InputStream in = null;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
 			pro = new Properties();
 			in = ClassLoader.getSystemResourceAsStream("db.properties");
 			pro.load(in);
-			
+
 			url = pro.getProperty("url");
 			username = pro.getProperty("username");
 			password = pro.getProperty("password");
-		} catch (IOException | ClassNotFoundException e) {
+
+			// oracle
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection oracleCon = DriverManager.getConnection(url, username, password);
+
+			// mysql
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection mysqlCon = DriverManager.getConnection(url, username, password);
+
+			// sqlite
+			Class.forName("org.sqlite.JDBC");
+			Connection sqliteCon = DriverManager.getConnection("jdbc:sqlite:./config/config.db");
+
+
+		} catch (IOException | ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		} finally{
 			try {
@@ -105,7 +117,6 @@ public class TestSQL {
 			ResultSet resultSet = statement.executeQuery(sql);
 			
 			while (resultSet.next()) {
-				
 				System.out.println(resultSet.getObject(1));
 			}
 			
@@ -116,42 +127,8 @@ public class TestSQL {
 			
 		}
 	}
-	
-	private String getSqlStr(int count,int times,String startTime,String endTime){
-		
-		StringBuilder sql = new StringBuilder();
-		sql.append("SELECT * FROM ( ");
-		sql.append(" SELECT ROWNUM AS ID, T.USER_NAME,T.CARD_NUM,T.AREA_CODE,");
-		sql.append(" CASE WHEN T.NEW_HETONG_NUM IS NULL THEN T.HETONG_NUM ELSE T.NEW_HETONG_NUM END AS CONTRACT_NUM,");
-		sql.append(" T.ID as FANGDAI_ID,");
-		sql.append("(select  userinfo.id from WIWJ_JR_USER userinfo where userinfo.user_name = t.user_name and userinfo.credentials_no = t.card_num and rownum = 1 ) as USER_ID,");
-		sql.append(" T.START_RENT_DATE as GMT_REPAYMENT_DATE,");
-		sql.append(" '2' AS DATA_TYPE, ");
-		sql.append(" (ADD_MONTHS(T.START_RENT_DATE,T.ZUQI)-1) AS GMT_HETONG_END ");
-		sql.append(" FROM WIWJ_FANGDAI T");
-//		sql.append(" INNER JOIN WIWJ_JR_USER USERINFO");
-//		sql.append(" ON USERINFO.USER_NAME = T.USER_NAME AND USERINFO.CREDENTIALS_NO = T.CARD_NUM");
-		sql.append(" WHERE T.STATUS IN ('6','10') ");
-//		sql.append(" AND NOT EXISTS (SELECT ATT.USER_ID FROM JR_ZUKE_ATTENTION ATT WHERE USERINFO.ID = ATT.USER_ID)");
-		sql.append(" AND NOT EXISTS (SELECT DANGER.FANGDAI_ID FROM WIWJ_ZHIMA_DANGER_LIST_DETAIL DANGER WHERE T.ID = DANGER.FANGDAI_ID AND DANGER.ZHIMA_DANGER_STATUS='10007001200010002')");
-		//sql.append(" AND NOT EXISTS (SELECT TUIZU.FANGDAI_ID from WIWJ_JR_ZHONGTUI TUIZU WHERE T.ID = TUIZU.FANGDAI_ID)");
-		sql.append(" AND T.RESERVED_FILED2 IS NULL AND (T.RESERVED_FILED3 IS NULL OR T.RESERVED_FILED3 = 'Y') ");
-		sql.append(" AND T.START_RENT_DATE >= to_timestamp('");
-		sql.append(startTime);
-		sql.append("','YYYY-MM-DD') ");
-		sql.append(" AND T.START_RENT_DATE <= to_timestamp('");
-		sql.append(endTime);
-		sql.append("','YYYY-MM-DD') ");
-		sql.append("  ORDER BY T.GMT_CREATE ) TEMP");
-		sql.append(" WHERE TEMP.ID BETWEEN ");
-		sql.append((1+(times*count)));
-		sql.append(" AND ");
-		sql.append((times+1)*count);
-		
-		return sql.toString();
-	}
-	
-	
+
+	// 自增主键
 	public void JDBC30() throws SQLException{
 		Statement statement2 = connection.createStatement();
 		statement2.executeUpdate("sql", Statement.RETURN_GENERATED_KEYS);
